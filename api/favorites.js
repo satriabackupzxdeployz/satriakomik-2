6c36eb3b85b5c42aa0580df0ec9e8feed27c2952
satriabackupzxdeployz <satriabackupz@gmail.com>
2026-04-19 16:23:14 +0700

    Add api/favorites.js

---
+++ api/favorites.js
@@ -0,0 +1,6 @@
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  res.json({ status: true, message: 'Favorites handled client-side' });
+};
